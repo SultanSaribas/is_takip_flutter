f844e91f5859adbebe378f87ac3a9ad368a2709f
atakandincer <atakan.dincer@outlook.com>
2020-10-06 17:25:53 +0300

    firestore_eklendi
    firestore_eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:is_takip_flutter/musteriler.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     home:MyApp() ,));
 }
