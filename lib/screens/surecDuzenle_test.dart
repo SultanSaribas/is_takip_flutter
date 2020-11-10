5e61d9821bc247fdb2eb30c58cfb454bd33a4365
esran-polat <platesranur@gmail.com>
2020-11-10 17:55:50 +0300

    listview yapısı henüz dinamik değil

--- lib/screens/surecDuzenle_test.dart
+++ lib/screens/surecDuzenle_test.dart
@@ -15,70 +15,99 @@ class _SurecDuzenleTestState extends State<SurecDuzenleTest> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   Future<String> _readProcess;
   var selectedCurrency;
+  var processSteps = [];
+  String tempProcess;
 
   @override
   void initState() {
     super.initState();
-    _readProcess = _readDocs();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Süreç Düzenleme"),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(35),
-          child: StreamBuilder<QuerySnapshot>(
-            stream: _firestore
-                .collection("/company/company_test_2/process")
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                Text("Yükleniyor!");
-              } else {
-                List<DropdownMenuItem> currencyItems = [];
-                for (int i = 0; i < snapshot.data.docs.length; i++) {
-                  DocumentSnapshot snap = snapshot.data.docs[i];
-                  currencyItems.add(DropdownMenuItem(
-                    child: Text(
-                      snap.data()["processName"],
+      appBar: AppBar(
+        title: Text("Süreç Düzenleme"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(35),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: _firestore
+              .collection("/company/company_test_2/process")
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              Text("Yükleniyor!");
+            } else {
+              List<DropdownMenuItem> currencyItems = [];
+              for (int i = 0; i < snapshot.data.docs.length; i++) {
+                DocumentSnapshot snap = snapshot.data.docs[i];
+                currencyItems.add(DropdownMenuItem(
+                  child: Text(
+                    snap.data()["processName"],
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  value: snap.data()["processID"],
+                ));
+              }
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  DropdownButton(
+                    items: currencyItems,
+                    onChanged: (currencyValue) {
+                      final snackbar = SnackBar(
+                          content: Text(
+                        "Düzenleme için seçilen süreç \"$currencyValue\"",
+                        style: TextStyle(color: Colors.blue),
+                      ));
+                      Scaffold.of(context).showSnackBar(snackbar);
+                      setState(() {
+                        selectedCurrency = currencyValue;
+                        tempProcess = selectedCurrency;
+                        debugPrint("Process ID :" + tempProcess);
+                        _readDocs();
+                      });
+                    },
+                    value: selectedCurrency,
+                    isExpanded: false,
+                    hint: new Text(
+                      "Düzenlenecek Süreci Seçiniz",
                       style: TextStyle(color: Colors.blue),
                     ),
-                    value: snap.data()["processName"],
-                  ));
-                }
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    DropdownButton(
-                      items: currencyItems,
-                      onChanged: (currencyValue) {
-                        final snackbar = SnackBar(
-                            content: Text(
-                          "Seçilen süreç $currencyValue ",
-                          style: TextStyle(color: Colors.blue),
-                        ));
-                        Scaffold.of(context).showSnackBar(snackbar);
-                        setState(() {
-                          selectedCurrency = currencyValue;
-                        });
-                      },
-                      value: selectedCurrency,
-                      isExpanded: false,
-                      hint: new Text(
-                        "Süreç Seçiniz",
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    )
-                  ],
-                );
-              }
-            },
-          ),
-        ));
+                  ),
+                  Expanded(child: _buildListView()),
+                ],
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 
-  Future<String> _readDocs() async {}
+  _readDocs() async {
+    DocumentSnapshot docsnap = await _firestore
+        .collection("/company/company_test_2/process")
+        .doc(tempProcess)
+        .get();
+    processSteps = (docsnap.data()["steps"]);
+    debugPrint(docsnap.data()["steps"].toString());
+    debugPrint("processStep kontrolü" + processSteps.toString());
+    debugPrint("adım sayısı " + processSteps.length.toString());
+    return _buildListView();
+  }
+
+  Widget _buildListView() {
+    debugPrint("hellooo " + processSteps.length.toString());
+    return ListView.builder(
+        itemCount: processSteps.length,
+        itemBuilder: (context, index) {
+          return Container(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[Text(processSteps[index])],
+              ));
+        });
+  }
 }
